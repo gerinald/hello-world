cc7e480ce87a75a91c1b4320a596f41b7f8f3fbd
gerinald <reginald.of.carter@gmail.com>
2017-01-30 14:13:50 -0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -5,8 +5,9 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut secret_number = rand::thread_rng().gen_range(1, 101);
 
+let mut  trys: i32=0;
 loop {
     println!("Please input your guess.");
 
@@ -18,13 +19,29 @@ loop {
         .expect("Please type a number!");
 
     println!("You guessed: {}", guess);
-
+    trys=trys+1;
+    println!("Your total guesses= {} ",trys);
     match guess.cmp(&secret_number) {
         Ordering::Less    => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
         Ordering::Equal   =>  {
                 println!("You win!");
+		println!("Do you want to play again? 1=yes 0=no");
+		let mut option = String::new();
+		    io::stdin().read_line(&mut option)
+        .expect("failed to read line");
+		let option : u32 = option.trim().parse()
+		     .expect("Do you want to play again?");
+		     
+		if option == 0 {
+		println!("GoodBye!");
                 break;
+		}
+		else {
+		secret_number = rand::thread_rng().gen_range(1, 101);
+		trys=0;
+		}
+		
             }
     }
 }
